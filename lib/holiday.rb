9b5247274ec30d0148d8e6da3208db112e74b507
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-12 04:25:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_supplies[:fall][:columbus_day] = "Flags"
 end
  holiday_supplies
-end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
