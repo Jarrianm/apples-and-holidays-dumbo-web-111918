21bf740b0483ceda798ba400705b2adc4fc58b77
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-12 04:42:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_winter_holiday_supplies(holiday_hash)
     end
   end
 end
-
+end
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
@@ -69,5 +69,4 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
 end
