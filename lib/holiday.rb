beb1d08f714752f1d6b97005b022acffad6ba6de
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-12 04:47:28 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,10 +41,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  holiday_supplies2 = {:fall => {
-    :columbus_day => ["Flags"]}}
-holiday_supplies.merge(holiday_supplies2)
-holiday_supplies
+   holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
